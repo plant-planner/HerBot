e7bcd0e3498f0bcfdcf1e62326bba6ff227f0238
Marius Grondahl <marius@grondahldesign.no>
2019-07-30 12:02:39 +0200

    Mergin midday Tuesday

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const Herb = require ('../models/Herb');
-const mongoose = require("mongoose");
+const Herb    = require ('../models/Herb');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
